136e773a6a7718243afdac5df435ec93c08dcb3d
Arafat Mamun <arafatmamun@hotmail.com>
2020-07-28 14:28:08 -0400

    initial

---
+++ src/test/java/TestReporting.java
@@ -0,0 +1,39 @@
+import io.qameta.allure.*;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+import static org.junit.Assert.fail;
+import static sun.nio.cs.Surrogate.is;
+
+/**
+ * author: Arafat Mamun
+ * username: amamun@freshdirect.com
+ * email: arafatmamun@hotmail.com
+ * date: 7/27/2020
+ * time: 11:44 AM
+ **/
+public class TestReporting {
+
+    @Test
+    @Epic("This is Epic")
+    @Feature("This is feature")
+    @Story("This is story")
+    public void simpleTest(){
+
+    }
+
+    @Test
+    @Epic("This is Epic")
+    @Feature("This is feature")
+    @Story("This is story")
+    public void simpleTest1() {
+
+    }
+
+
+
+
+}
